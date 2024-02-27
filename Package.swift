3917668ca36e155949cbaf5a57d646e0c0776b80
Berkay Ünal <berkay.unal579@gmail.com>
2024-02-27 10:34:43 +0300

    refactor(): remove test folder

--- Package.swift
+++ Package.swift
@@ -16,9 +16,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CustomHorizontalCollectionViewLayoutDesigns"),
-        .testTarget(
-            name: "CustomHorizontalCollectionViewLayoutDesignsTests",
-            dependencies: ["CustomHorizontalCollectionViewLayoutDesigns"]),
+            name: "CustomHorizontalCollectionViewLayoutDesigns")
+
     ]
 )
